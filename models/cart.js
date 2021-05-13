9a00c7d8ba89bfb9591095cde60b7c6278560108
raudelfg <raudelfg@gmail.com>
2021-05-13 09:37:35 -0400

    edit and update product

---
+++ models/cart.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const Product = require('./product');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        // Fetch previous Cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+            // Analyze cart / Find existing Product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            if (existingProduct) {
+                updatedProduct = {...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                updatedProduct = { id: id, qty: 1 };
+                cart.products = [...cart.products, updatedProduct];
+            }
+            // if (existingProductIndex === -1) {
+            //     cart.products = [...cart.products, {id: id, qty: 1}];
+            // } else {
+            //     cart.products[existingProductIndex].qty += 1;
+            // }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        });
+    }
+};
